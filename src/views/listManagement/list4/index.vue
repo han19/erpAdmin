2c67df14c0f8204f1ef3447f374948a016703bca
汉先生 <351468390@qq.com>
2019-12-03 17:14:29 +0800

    geshihuadai

--- src/views/listManagement/list4/index.vue
+++ src/views/listManagement/list4/index.vue
@@ -18,50 +18,24 @@
 				</el-select>
 			</div>
 			<div class="item">
-				<el-input placeholder="请输入内容" v-model="input3" class="input-with-select">
-					<el-button slot="append" icon="el-icon-search"></el-button>
-				</el-input>
+				<el-input placeholder="请输入内容" v-model="input3" class="input-with-select"><el-button slot="append" icon="el-icon-search"></el-button></el-input>
 			</div>
 		</div>
-	    <div class="content-con">
-			<el-table
-			    :data="tableData"
-			    border
-			    style="width: 100%">
-			    <el-table-column
-			      fixed
-			      prop="date"
-			      label="日期">
-			    </el-table-column>
-			    <el-table-column
-			      prop="name"
-			      label="姓名">
-			    </el-table-column>
-			    <el-table-column
-			      prop="province"
-			      label="省份">
-			    </el-table-column>
-			    <el-table-column
-			      prop="city"
-			      label="市区">
-			    </el-table-column>
-			    <el-table-column
-			      prop="address"
-			      label="地址">
-			    </el-table-column>
-			    <el-table-column
-			      prop="zip"
-			      label="邮编">
-			    </el-table-column>
-			    <el-table-column
-			      fixed="right"
-			      label="操作">
-			      <template slot-scope="scope">
-			        <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
-			        <el-button type="text" size="small">编辑</el-button>
-			      </template>
-			    </el-table-column>
-			  </el-table>
+		<div class="content-con">
+			<el-table :data="tableData" border style="width: 100%">
+				<el-table-column fixed prop="date" label="日期"></el-table-column>
+				<el-table-column prop="name" label="姓名"></el-table-column>
+				<el-table-column prop="province" label="省份"></el-table-column>
+				<el-table-column prop="city" label="市区"></el-table-column>
+				<el-table-column prop="address" label="地址"></el-table-column>
+				<el-table-column prop="zip" label="邮编"></el-table-column>
+				<el-table-column fixed="right" label="操作">
+					<template slot-scope="scope">
+						<el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+						<el-button type="text" size="small">编辑</el-button>
+					</template>
+				</el-table-column>
+			</el-table>
 		</div>
 	</div>
 </template>
@@ -126,42 +100,45 @@ export default {
 			],
 			value1: '',
 			input3: '',
-			tableData: [{
-			          date: '2016-05-02',
-			          name: '王小虎',
-			          province: '上海',
-			          city: '普陀区',
-			          address: '上海市普陀区金沙江路 1518 弄',
-			          zip: 200333
-			        }, {
-			          date: '2016-05-04',
-			          name: '王小虎',
-			          province: '上海',
-			          city: '普陀区',
-			          address: '上海市普陀区金沙江路 1517 弄',
-			          zip: 200333
-			        }, {
-			          date: '2016-05-01',
-			          name: '王小虎',
-			          province: '上海',
-			          city: '普陀区',
-			          address: '上海市普陀区金沙江路 1519 弄',
-			          zip: 200333
-			        }, {
-			          date: '2016-05-03',
-			          name: '王小虎',
-			          province: '上海',
-			          city: '普陀区',
-			          address: '上海市普陀区金沙江路 1516 弄',
-			          zip: 200333
-			        }]
+			tableData: [
+				{
+					date: '2016-05-02',
+					name: '王小虎',
+					province: '上海',
+					city: '普陀区',
+					address: '上海市普陀区金沙江路 1518 弄',
+					zip: 200333
+				},
+				{
+					date: '2016-05-04',
+					name: '王小虎',
+					province: '上海',
+					city: '普陀区',
+					address: '上海市普陀区金沙江路 1517 弄',
+					zip: 200333
+				},
+				{
+					date: '2016-05-01',
+					name: '王小虎',
+					province: '上海',
+					city: '普陀区',
+					address: '上海市普陀区金沙江路 1519 弄',
+					zip: 200333
+				},
+				{
+					date: '2016-05-03',
+					name: '王小虎',
+					province: '上海',
+					city: '普陀区',
+					address: '上海市普陀区金沙江路 1516 弄',
+					zip: 200333
+				}
+			]
 		};
 	},
 	methods: {
-	      handleClick(row) {
-	        console.log(row);
-	      }
-	    },
+		handleClick(row) {}
+	}
 };
 </script>
 
@@ -174,7 +151,7 @@ export default {
 		margin-right: 30px;
 	}
 }
-.content-con{
- padding:10px;	
+.content-con {
+	padding: 10px;
 }
 </style>
